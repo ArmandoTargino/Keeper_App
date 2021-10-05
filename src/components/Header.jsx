6d4185d4936c23c474664f2ff173942835e48eae
ArmandoTargino <43214639+ArmandoTargino@users.noreply.github.com>
2021-10-04 21:10:02 -0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        Keeper <i class="far fa-sticky-note"></i>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
